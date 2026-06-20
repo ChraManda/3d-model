8a8e0db8933893d85b50c505add64072e4cbda07
Chradoreng M Sangma <125442119+ChraManda@users.noreply.github.com>
2026-06-20 10:34:21 +0530

    refractor the the whole system

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,5 +1,5 @@
-import { getAllModels } from "../lib/models";
-import type { Model } from "@/app/types"
+import { getAllModels } from "@/lib/models";
+import type { Model } from "@/types"
 export default async function () {
   const models: Model[] = await getAllModels();
   return models.map(model => <p key={model.id}>{model.name}</p>)
